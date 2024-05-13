13a6d0e5dac8a1cb7866cb84d955708cbe8bf0d3
Yeshua Espinoza <masterfx_02@icloud.com>
2024-05-13 11:50:34 -0600

    Imágenes del sitio

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -3,14 +3,14 @@
             <div class="row">
                <div class="col-md-5">
                   <div class="titlepage">
-                     <h2>About Us</h2>
-                     <p>The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum. </p>
-                     <a class="read_more" href="Javascript:void(0)"> Read More</a>
+                     <h2>Sobre Nosotros</h2>
+                     <p>En ABIIOR Studios, nos enorgullece ofrecer un espacio de grabación de vanguardia diseñado para inspirar y elevar la creatividad de nuestros clientes. Con un equipo apasionado y experimentado de ingenieros de sonido y productores, estamos comprometidos a brindar resultados excepcionales que superen las expectativas.</p>
+                     <a class="read_more" href="Javascript:void(0)">Leer Más</a>
                   </div>
                </div>
                <div class="col-md-7">
                   <div class="about_img">
-                     <figure><img src="images/about.png" alt="#"/></figure>
+                     <figure><img src="images/about.jpg" alt="#"/></figure>
                   </div>
                </div>
             </div>
